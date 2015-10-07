2a8da275f1555dd4bdeeaabe840584d269408ac8
Tadi <adam.tomko@gmail.com>
2015-10-07 20:37:02 +0200

    added templating and a test controller

---
+++ src/main/java/restaurant/controller/Test.java
@@ -0,0 +1,19 @@
+package restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Test {
+
+    @RequestMapping(name = "/", method = RequestMethod.GET)
+    public String test(Model model){
+
+        model.addAttribute("name","world");
+
+        return "index";
+    }
+
+}
