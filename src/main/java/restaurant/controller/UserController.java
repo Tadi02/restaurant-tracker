8e9aec06512b15be94efc9c0afe8727458808588
Tadi <adam.tomko@gmail.com>
2015-11-07 15:26:34 +0100

    user listing page #11

--- src/main/java/restaurant/controller/UserController.java
+++ src/main/java/restaurant/controller/UserController.java
@@ -1,19 +1,22 @@
 package restaurant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import restaurant.domain.User;
 import restaurant.dto.RegisteringUser;
 import restaurant.service.UserService;
 
 import javax.validation.Valid;
 
 @Controller
-public class SecurityController {
+public class UserController {
 
     @Autowired
     UserService userService;
@@ -49,6 +52,19 @@ public class SecurityController {
     String getRegisterSuccessPage() {
 
         return "registration_success";
+    }
+
+    @RequestMapping(value = "/admin/users")
+    String getUserAdminPage(Model model, @RequestParam(value = "page", required = false, defaultValue = "0") int page){
+        Page<User> users = userService.getUsersPage(page);
+        boolean isLast = false;
+        if(users.getTotalPages() == (page + 1)){
+            isLast = true;
+        }
+        model.addAttribute("users",users);
+        model.addAttribute("isLast",isLast);
+        model.addAttribute("current",page);
+        return "users";
     }
 
 }
